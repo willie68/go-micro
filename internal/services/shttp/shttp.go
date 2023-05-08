cf48d08e6788bd62abb2cd1238cb9b55485ee9d3
Wilfried Klaas <w.klaas@gmx.de>
2023-05-08 10:02:18 +0200

    removed unnecessary code

--- internal/services/shttp/shttp.go
+++ internal/services/shttp/shttp.go
@@ -2,14 +2,25 @@ package shttp
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"net"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/samber/do"
 	"github.com/willie68/go-micro/internal/config"
-	"github.com/willie68/go-micro/internal/crypt"
 	log "github.com/willie68/go-micro/internal/logging"
 )
 
@@ -73,7 +84,7 @@ func (s *SHttp) ShutdownServers() {
 }
 
 func (s *SHttp) startHTTPSServer(router *chi.Mux) {
-	gc := crypt.GenerateCertificate{
+	gc := GenerateCertificate{
 		Organization: "MCS",
 		Host:         "127.0.0.1",
 		ValidFor:     10 * 365 * 24 * time.Hour,
@@ -116,4 +127,123 @@ func (s *SHttp) startHTTPServer(router *chi.Mux) {
 			log.Logger.Alertf("error starting server: %s", err.Error())
 		}
 	}()
+}
+
+// GenerateCertificate model
+type GenerateCertificate struct {
+	Organization string
+	Host         string
+	ValidFrom    string
+	ValidFor     time.Duration
+	IsCA         bool
+	RSABits      int
+	EcdsaCurve   string
+	Ed25519Key   bool
+}
+
+func (gc *GenerateCertificate) publicKey(priv any) any {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &k.PublicKey
+	case *ecdsa.PrivateKey:
+		return &k.PublicKey
+	case ed25519.PrivateKey:
+		return k.Public().(ed25519.PublicKey)
+	default:
+		return nil
+	}
+}
+
+// GenerateTLSConfig generates the config
+func (gc *GenerateCertificate) GenerateTLSConfig() (*tls.Config, error) {
+	var priv any
+	var err error
+	switch gc.EcdsaCurve {
+	case "":
+		if gc.Ed25519Key {
+			_, priv, err = ed25519.GenerateKey(rand.Reader)
+		} else {
+			priv, err = rsa.GenerateKey(rand.Reader, gc.RSABits)
+		}
+	case "P224":
+		priv, err = ecdsa.GenerateKey(elliptic.P224(), rand.Reader)
+	case "P256":
+		priv, err = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	case "P384":
+		priv, err = ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	case "P521":
+		priv, err = ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	default:
+		log.Logger.Fatalf("Unrecognized elliptic curve: %q", gc.EcdsaCurve)
+		return nil, err
+	}
+	if err != nil {
+		log.Logger.Fatalf("Failed to generate private key: %v", err)
+		return nil, err
+	}
+
+	var notBefore time.Time
+	if len(gc.ValidFrom) == 0 {
+		notBefore = time.Now()
+	} else {
+		notBefore, err = time.Parse("Jan 2 15:04:05 2006", gc.ValidFrom)
+		if err != nil {
+			log.Logger.Fatalf("Failed to parse creation date: %v", err)
+			return nil, err
+		}
+	}
+
+	notAfter := notBefore.Add(gc.ValidFor)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		log.Logger.Fatalf("Failed to generate serial number: %v", err)
+		return nil, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{gc.Organization},
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	hosts := strings.Split(gc.Host, ",")
+	for _, h := range hosts {
+		if ip := net.ParseIP(h); ip != nil {
+			template.IPAddresses = append(template.IPAddresses, ip)
+		} else {
+			template.DNSNames = append(template.DNSNames, h)
+		}
+	}
+
+	if gc.IsCA {
+		template.IsCA = true
+		template.KeyUsage |= x509.KeyUsageCertSign
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, gc.publicKey(priv), priv)
+	if err != nil {
+		log.Logger.Fatalf("Failed to create certificate: %v", err)
+		return nil, err
+	}
+
+	privBytes, err := x509.MarshalPKCS8PrivateKey(priv)
+	if err != nil {
+		log.Logger.Fatalf("Unable to marshal private key: %v", err)
+		return nil, err
+	}
+
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: privBytes})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+
+	return &tls.Config{Certificates: []tls.Certificate{tlsCert}}, nil
 }
